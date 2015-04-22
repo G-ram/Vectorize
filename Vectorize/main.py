b317ef7715a5ecc36b8339ff4f6a405d678d5e5f
Brian Donghee Shin <ds2791@columbia.edu>
2015-04-22 19:30:29 -0400

    Add processor and full pipeline test

--- Vectorize/main.py
+++ Vectorize/main.py
@@ -2,10 +2,15 @@ import numpy
 import pyopencl
 import os
 import argparse
+
 from video import Video
+from processor import Processor
 
 def main(file_path):
     video = Video(file_path)
+
+    processor = Processor()
+    processor.process(video)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Raster to SVG converter')
---
+++ Vectorize/processor.py
@@ -0,0 +1,8 @@
+from reikna import cluda
+
+class Processor:
+    def __init__(self):
+        pass
+
+    def process(self, video):
+        pass
