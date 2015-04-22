385cf7a31af9c2289e894e4cb78cfa0833f9ca4f
G-Ram <ggobieski@yahoo.com>
2015-04-22 18:16:05 -0400

    video file

--- Vectorize/main.py
+++ Vectorize/main.py
@@ -1,25 +1,18 @@
 import numpy
 import pyopencl
-import cv2
-import os, sys
+import os
 import argparse
+from video import Video
 
 def main(file_path):
-	frames = []
-	frame_count = 0
-	flag = True
-	cap = cv2.VideoCapture(file_path)
-	while flag:
-		flag, frame = cap.read()
-		frames += [frame]
-		frame_count += 1
+    video = Video(file_path)
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Raster to SVG converter')
-	parser.add_argument('file')
-	args = parser.parse_args()
-	print args.file
-	if os.path.isfile(args.file) :
-		main(args.file)
-	else:
-		print "file specified does not exist"
+    parser = argparse.ArgumentParser(description='Raster to SVG converter')
+    parser.add_argument('file')
+    args = parser.parse_args()
+    print args.file
+    if os.path.isfile(args.file) :
+        main(args.file)
+    else:
+        print "file specified does not exist"
