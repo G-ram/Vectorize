2279a4df4fbfbb85b64e19145f3940e5dd406c73
Brian Donghee Shin <ds2791@columbia.edu>
2015-04-28 16:44:25 -0400

    Add CUDA macros

--- src/struct.h
+++ src/struct.h
@@ -1,6 +1,20 @@
 #ifndef __STRUCT_H__
 #define __STRUCT_H__
 
+#define CUDA_CALL_SETUP cudaError_t cudaStatus;\
+	bool hasErrored = false;
+
+#define CUDA_CALL(call, error)\
+  cudaStatus = call;\
+  if (cudaStatus != cudaSuccess) {\
+    fprintf(stderr, error);\
+    fprintf(stderr, " - reason: %s\n", cudaGetErrorString(cudaStatus));\
+    if (!hasErrored) {\
+    	hasErrored = true;\
+    	goto Error;\
+    }\
+  }
+
 struct pixel {
 	unsigned char r;
 	unsigned char g;
